703a5f6787504b6ca316387f01669eecf85f5a9a
Esteban Barboza <edbarboza20@gmail.com>
2023-01-12 20:34:54 -0600

    Refactoring products to use deferences environment

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,11 +1,24 @@
 import faker from 'faker';
 
-let products = '';
 
-for(let i = 0; i < 6; i++){
-    const name = faker.commerce.product();
-    products += `<div>${name}</div>`;
+const mount = (element) =>{
+    let products = '';
 
+    for(let i = 0; i < 6; i++){
+        const name = faker.commerce.product();
+        products += `<div>${name}</div>`;
+
+    }
+
+    element.innerHTML = products;
+};
+
+if(process.env.NODE_ENV === 'development') {
+    const element = document.querySelector('#dev-products');
+
+    if(element){
+        mount(element);
+    }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+export { mount };
