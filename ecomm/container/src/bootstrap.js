d7d78dc3b50066cdc4f4a4d3b8fdef09cbe0f7c3
Esteban Barboza <edbarboza20@gmail.com>
2023-01-16 15:19:02 -0600

    Finish isolation projects

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,6 +1,7 @@
-import {mount} from 'products/productsIndex';
-import 'cart/cartShow';
+import {mount as productMount} from 'products/productsIndex';
+import {mount as cartMount}  from 'cart/cartShow';
 
 console.log('Container');
 
-mount(document.querySelector("#dev-products"));
+productMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
