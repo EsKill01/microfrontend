85af0971d1cb0f0c5e4b4574ee8f30a1eafa545e
Esteban Barboza <edbarboza20@gmail.com>
2023-01-23 14:17:26 -0600

    shared references from react and react-dom

--- mfp/packages/marketing/config/webpack.dev.js
+++ mfp/packages/marketing/config/webpack.dev.js
@@ -18,6 +18,7 @@ const devConfig = {
             exposes:{
                 './MarketingApp': './src/bootstrap'
             },
+            shared: ['react', 'react-dom']
         }),
         new HtmlWebPackPlugin({
             template: './public/index.html'
